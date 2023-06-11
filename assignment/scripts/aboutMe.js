d351339c99ee469de40009c861ec0f3f59777528
Adam Myhand <rhematicmovement@gmail.com>
2023-06-11 10:43:13 -0500

    answered 15

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -67,12 +67,23 @@ if (luckyNumber === 2 && adventurous){
     console.log("Roll the dice!");
 } else{console.log("What a boring boy.");}
 
-
 // 15 - Write a conditional that console logs "I can have more pets!" 
 //if the value of `pets` is less than the value of `allowedPets`,
 // console logs "I have enough pets" if the value of `pets` is equal to the value of `allowedPets`,
 // and console logs "Oh no, I have too many pets!"
 // if the value of `pets` is greater than the value of `allowedPets`.
+
+if (pets < allowedPets) {
+    console.log("I can have more pets");
+}
+
+else if (pets === allowedPets) {
+    console.log("I have enough pets.");
+} 
+
+else if (pets > allowedPets) {
+    console.log("Oh no, I have too many pets!");
+}
 
 // STRETCH GOALS:
 
