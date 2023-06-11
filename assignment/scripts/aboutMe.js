78593b1ea542365140fb3e871fe03efc5dd536d1
Adam Myhand <rhematicmovement@gmail.com>
2023-06-11 10:58:08 -0500

    formatted code

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,11 +1,11 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
 
-let firstName = 'Adam';
+let firstName = "Adam";
 
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
 
-let lastName = 'Myhand';
+let lastName = "Myhand";
 
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
@@ -20,11 +20,21 @@ console.log(fullName);
 
 let luckyNumber = 7;
 
-// 6 - Console log this sentence, adding in the variables you created above: 
+// 6 - Console log this sentence, adding in the variables you created above:
 // 'My name is (full name), and I think (lucky number) is a winner!'.
 // Refer back to the videos if you need help with this one.
 
-console.log('My name is' + " " + fullName + " " + 'and I think' + " " + luckyNumber + " " + 'is a winner!');
+console.log(
+  "My name is" +
+    " " +
+    fullName +
+    " " +
+    "and I think" +
+    " " +
+    luckyNumber +
+    " " +
+    "is a winner!"
+);
 
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
 
@@ -32,7 +42,7 @@ let adventurous = false;
 
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
 
-let food = 'ramen';
+let food = "ramen";
 
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
 
@@ -53,50 +63,54 @@ console.log(pets);
 
 const allowedPets = 2;
 
-// 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
+// 13 - Create a conditional: if adventurous is true, console log "Adventures are great!",
 // if it's not true, console log "How about we stay home?"
 
 if (adventurous) {
-    console.log("Adventures are great!");
- } else {console.log("How about we stay home?");} 
+  console.log("Adventures are great!");
+} else {
+  console.log("How about we stay home?");
+}
 
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
 
-if (luckyNumber === 2 && adventurous){
-    console.log("Roll the dice!");
-} else{console.log("What a boring boy.");}
+if (luckyNumber === 2 && adventurous) {
+  console.log("Roll the dice!");
+} else {
+  console.log("What a boring boy.");
+}
 
-// 15 - Write a conditional that console logs "I can have more pets!" 
+// 15 - Write a conditional that console logs "I can have more pets!"
 //if the value of `pets` is less than the value of `allowedPets`,
 // console logs "I have enough pets" if the value of `pets` is equal to the value of `allowedPets`,
 // and console logs "Oh no, I have too many pets!"
 // if the value of `pets` is greater than the value of `allowedPets`.
 
 if (pets < allowedPets) {
-    console.log("I can have more pets");
-}
-
-else if (pets === allowedPets) {
-    console.log("I have enough pets.");
-} 
-
-else if (pets > allowedPets) {
-    console.log("Oh no, I have too many pets!");
+  console.log("I can have more pets");
+} else if (pets === allowedPets) {
+  console.log("I have enough pets.");
+} else if (pets > allowedPets) {
+  console.log("Oh no, I have too many pets!");
 }
 
 let ifIHadMoreTime = false;
 
 if (ifIHadMoreTime) {
-    console.log("I'm going to do the stretch goals");  
-} else {console.log("Time to run errands and pack for a 4 day work conference in Bemidji");}
+  console.log("I'm going to do the stretch goals");
+} else {
+  console.log(
+    "Time to run errands and pack for a 4 day work conference in Bemidji"
+  );
+}
 
 // STRETCH GOALS:
 
 // 16 - Make a variable called `mostPets` and a conditional that
 // correctly checks the `pets` and `friendsPets` variables, and
 // assigns the highest value to `mostPets`. There's several possibilities --
-// be sure to think through all the scenarios. 
+// be sure to think through all the scenarios.
 // console.log `mostPets` after the conditional has run.
 
 // 17 - Write a *switch* statement that logs:
@@ -107,5 +121,3 @@ if (ifIHadMoreTime) {
 //      You'll need to research how to use switch statements!
 
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
-
-
